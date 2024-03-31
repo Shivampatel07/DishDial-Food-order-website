0439424ba90a87f9b30672947cefa2b1c60a9b67
shivam <shivp72019@gmail.com>
2024-03-31 12:49:46 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,22 @@ const AuthRouter = require("./src/routes/Authentication.route");
 const cookieParser = require("cookie-parser");
 const restaurentRouter = require("./src/routes/FindRestaurent.route");
 const path = require("path");
-const frontend_url = process.env.FRONTEND_BASE_URL
+const NODE_ENV = process.env.ENVIRONMENT
+const frontend_url = NODE_ENV === 'production' ? process.env.FRONTEND_BASE_URL : ('http://localhost:' + process.env.PORT)
 connectDB();
 app.use(cookieParser({}));
-app.use(cors({origin: frontend_url,credentials: true}));
-console.log(frontend_url)
+app.use(cors({ origin: frontend_url, credentials: true }));
+
+console.log("Node environment:", NODE_ENV)
+console.log("Frontend URL:", frontend_url)
+
 app.use(
   "/images/restaurant",
-  expr.static(path.join(__dirname, process.env.ENVIRONMENT === 'production' ? "../src/Images/restaurant" : "./src/Images/restaurant"))
+  expr.static(path.join(__dirname, NODE_ENV === 'production' ? "../src/Images/restaurant" : "./src/Images/restaurant"))
 );
 app.use(
   "/images/product",
-  expr.static(path.join(__dirname, process.env.ENVIRONMENT === 'production' ? "../src/Images/product" : "./src/Images/product"))
+  expr.static(path.join(__dirname, NODE_ENV === 'production' ? "../src/Images/product" : "./src/Images/product"))
 );
 
 app.use(expr.json());
