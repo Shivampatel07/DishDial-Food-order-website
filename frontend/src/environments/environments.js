026b4f70a1617ebfc6f9380f121b27f20dd7f80c
shivam <shivp72019@gmail.com>
2024-03-31 12:07:30 +0530

    updated

--- frontend/src/environments/environments.js
+++ frontend/src/environments/environments.js
@@ -1,7 +1,8 @@
+const frontend_url = process.env.REACT_APP_CMS_BACKEND_BASE_URL
 const configureData = {
-  baseUrl: "https://food-ordering-website-4bfm.onrender.com",
-  restaurantImage: "https://food-ordering-website-4bfm.onrender.com/images/restaurant",
-  productImage: "https://food-ordering-website-4bfm.onrender.com/images/product",
+  baseUrl: frontend_url,
+  restaurantImage: frontend_url + "/images/restaurant",
+  productImage: frontend_url + "/images/product",
 };
 
 export default configureData;
