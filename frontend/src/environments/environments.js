eeb241e8de741eb32ca7f030a49952b73e60d76f
shivam <shivp72019@gmail.com>
2024-08-01 00:30:29 +0530

    remove console

--- frontend/src/environments/environments.js
+++ frontend/src/environments/environments.js
@@ -1,6 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV
 const frontend_url = NODE_ENV === 'production' ? process.env.REACT_APP_CMS_BACKEND_BASE_URL : 'http://localhost:8080'
-console.log(process.env.NODE_ENV)
+
 const configureData = {
   baseUrl: frontend_url,
   restaurantImage: frontend_url + "/images/restaurant",
